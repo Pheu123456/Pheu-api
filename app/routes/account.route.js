bb032285a95137a8ab635b276317e728d06b8187
21Pheu12 <phu.tan.azx@gmail.com>
2023-08-19 09:25:54 +0700

    api books update

--- app/routes/account.route.js
+++ app/routes/account.route.js
@@ -5,6 +5,83 @@
 const accountService = require('../services/account.service');
 
 module.exports = function (app) {
+  /**
+     * @api {GET} /api/v1/accounts/:id Get All
+     * @apiVersion 0.4.0
+     * @apiName getAll
+     * @apiGroup Accounts
+     * @apiPermission Every type of user
+     *
+     * @apiDescription Get all account
+     *
+     * @apiParam {string} id ID of account, on params
+     *
+     * @apiExample Example usage:
+     * curl -i http://localhost:443/api/v1/accounts/2
+     *
+     * @apiSuccess {String} id the ID of account
+     * @apiSuccess {String} title title of account
+     *
+     * @apiSuccessExample Success-Response:
+     *     HTTP/1.1 200 OK
+     *     {
+     *          "data":{
+     *              "id": "2",
+     *              ...
+     *          },
+     *          "result": "ok",
+     *          "message" ""
+     *     }
+     *
+     * @apiError invalid input data
+     *
+     * @apiErrorExample Error-Response:
+     *     HTTP/1.1 400 Bad Request
+     *     {
+     *       "result": "fail",
+     *       "message": "invalid input"
+     *     }
+     */
+  app.get('/api/v1/auth/accounts/:id', accountService.getOne);
+  app.post('/api/v1/accounts/login', accountService.login);
+  app.post('/api/v1/accounts/register', accountService.register);
+  /**
+   * @api {POST} /api/v1/accounts Create One
+   * @apiVersion 0.4.0
+   * @apiName createOne
+   * @apiGroup Accounts
+   * @apiPermission Every type of user
+   *
+   * @apiDescription Create one account
+   *
+   * @apiBody {String} login_name the unique name of user for login
+   * @apiBody {String} full_name the name of user.
+   * @apiBody {String} password password of user
+   *
+   * @apiExample Example usage:
+   * curl -i http://localhost:443/api/v1/accounts
+   *
+   *
+   * @apiSuccessExample Success-Response:
+   *     HTTP/1.1 200 OK
+   *     {
+   *          "data":{
+   *              "id": "2",
+   *              ...
+   *          },
+   *          "result": "ok",
+   *          "message" ""
+   *     }
+   *
+   * @apiError invalid input data
+   *
+   * @apiErrorExample Error-Response:
+   *     HTTP/1.1 400 Bad Request
+   *     {
+   *       "result": "fail",
+   *       "message": "invalid input"
+   *     }
+   */    
     app.get('/api/v1/auth/accounts', accountService.getAll);
     /**
      * @api {GET} /api/v1/accounts/:id Get One
@@ -18,7 +95,7 @@ module.exports = function (app) {
      * @apiParam {string} id ID of account, on params
      *
      * @apiExample Example usage:
-     * curl -i http://localhost:3000/api/v1/accounts/2
+     * curl -i http://localhost:443/api/v1/accounts/2
      *
      * @apiSuccess {String} id the ID of account
      * @apiSuccess {String} title title of account
--- app/routes/book.route.js
+++ app/routes/book.route.js
@@ -6,6 +6,7 @@ const bookService = require('../services/book.service');
 
 module.exports = function (app) {
     app.get('/api/v1/books', bookService.getAll);
+
     app.get('/api/v1/books/:id', bookService.getOne);
     app.post('/api/v1/auth/books', bookService.create);
     app.put('/api/v1/auth/books/:id', bookService.update);
